a92b6c74907c93d06357bc0a2aa5919f589cf731
thi <thint2908@gmail.com>
2025-02-04 20:52:49 +0700

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline text-red-500">Thi portfolio</h1>
+			<h1 className="text-3xl font-bold underline text-red-500">Thi Nguyen Dev</h1>
 		</>
 	)
 }
